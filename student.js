eff5591e8e23546488701c8fbae776b9719a081e
a104494 <a1044941922@gmail.com>
2019-06-24 21:51:42 +0800

    login for student

--- student.js
+++ student.js
@@ -19,7 +19,7 @@ exports.save = function(student,callback){
             return callback(err)
         }
         var students = JSON.parse(data).students
-        // student.id = students[students.length-1].id+1//id唯一
+        student.id = students[students.length-1].id+1//id唯一
        students.push(student)
         var fileData=JSON.stringify({
             students:students
